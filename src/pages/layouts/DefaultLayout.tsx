14a4734df6df9e5aef5e2156da8b44486ed0d9cc
Alex <a.gachegov@agatgroup.com>
2024-06-05 15:29:28 +0300

    Add universal layout

--- src/pages/layouts/DefaultLayout.tsx
+++ src/pages/layouts/DefaultLayout.tsx
@@ -2,12 +2,14 @@ import { FC, ReactNode } from 'react';
 import styles from './layouts.module.scss';
 interface DefaultLayoutProps {
   children: ReactNode;
+  classNameContainer?: string;
+  classNameMain?: string;
 }
 
-const DefaultLayout: FC<DefaultLayoutProps> = ({ children }) => {
+const DefaultLayout: FC<DefaultLayoutProps> = ({ children, classNameMain = '', classNameContainer = '' }) => {
   return (
-    <div className={styles['default-layout']}>
-      <main className={styles['chat-page']}>{children}</main>
+    <div className={`${classNameContainer}`}>
+      <main className={`${classNameMain}`}>{children}</main>
     </div>
   );
 };
