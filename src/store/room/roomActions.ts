8cfeca8bff75be654e4834d34c3d4b672a17601f
Alex <a.gachegov@agatgroup.com>
2024-06-15 12:01:44 +0300

    add store room

---
+++ src/store/room/roomActions.ts
@@ -0,0 +1,64 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import RoomService from '@/services/RoomService';
+import { CreateRoomParams, InviteUserParams } from '@/models/Room';
+import { roomConfig } from '../config';
+
+export const createRoom = createAsyncThunk(
+  roomConfig.room.createRoom,
+  async ({ name, userId }: CreateRoomParams, { rejectWithValue }) => {
+    try {
+      const response = await RoomService.createRoom(name, userId);
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const inviteUserToRoom = createAsyncThunk(
+  roomConfig.room.inviteUser,
+  async ({ roomId, userId }: InviteUserParams, { rejectWithValue }) => {
+    try {
+      const response = await RoomService.inviteUserToRoom(roomId, userId);
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const leaveRoom = createAsyncThunk(
+  roomConfig.room.leaveRoom,
+  async ({ roomId, userId }: InviteUserParams, { rejectWithValue }) => {
+    try {
+      const response = await RoomService.leaveRoom(roomId, userId);
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const deleteRoom = createAsyncThunk(
+  roomConfig.room.deleteRoom,
+  async ({ roomId, userId }: InviteUserParams, { rejectWithValue }) => {
+    try {
+      const response = await RoomService.deleteRoom(roomId, userId);
+      return { roomId: response.data.roomId };
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const getRoomsForUser = createAsyncThunk(
+  roomConfig.room.getRooms,
+  async (userId: string, { rejectWithValue }) => {
+    try {
+      const response = await RoomService.getRoomsForUser(userId);
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
---
+++ src/store/room/roomSlice.ts
@@ -0,0 +1,86 @@
+import { Room, RoomState } from '@/models/Room';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { createRoom, deleteRoom, getRoomsForUser, inviteUserToRoom, leaveRoom } from './roomActions';
+
+const initialState: RoomState = {
+  rooms: [],
+  isLoading: false,
+  error: null,
+};
+
+const roomSlice = createSlice({
+  name: 'room',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // createRoom
+      .addCase(createRoom.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createRoom.fulfilled, (state, action: PayloadAction<Room>) => {
+        state.rooms.push(action.payload);
+        state.isLoading = false;
+      })
+      .addCase(createRoom.rejected, (state, action: PayloadAction<any>) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      // inviteUserToRoom
+      .addCase(inviteUserToRoom.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(inviteUserToRoom.fulfilled, (state, action: PayloadAction<Room>) => {
+        const index = state.rooms.findIndex((room) => room._id === action.payload._id);
+        if (index !== -1) {
+          state.rooms[index] = action.payload;
+        }
+        state.isLoading = false;
+      })
+      .addCase(inviteUserToRoom.rejected, (state, action: PayloadAction<any>) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      // leaveRoom
+      .addCase(leaveRoom.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(leaveRoom.fulfilled, (state, action: PayloadAction<Room>) => {
+        const index = state.rooms.findIndex((room) => room._id === action.payload._id);
+        if (index !== -1) {
+          state.rooms[index] = action.payload;
+        }
+        state.isLoading = false;
+      })
+      .addCase(leaveRoom.rejected, (state, action: PayloadAction<any>) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      // deleteRoom
+      .addCase(deleteRoom.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteRoom.fulfilled, (state, action: PayloadAction<{ roomId: string }>) => {
+        state.rooms = state.rooms.filter((room) => room._id !== action.payload.roomId);
+        state.isLoading = false;
+      })
+      .addCase(deleteRoom.rejected, (state, action: PayloadAction<any>) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      // getRoomsForUser
+      .addCase(getRoomsForUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getRoomsForUser.fulfilled, (state, action: PayloadAction<Room[]>) => {
+        state.rooms = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(getRoomsForUser.rejected, (state, action: PayloadAction<any>) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default roomSlice.reducer;
